3d0e7714480503b343f51f251eceb54c91f8e302
Hyeok22 <wnsgur347@gmail.com>
2025-02-21 15:20:19 +0900

    프로젝트 구성

---
+++ backend/src/test/java/com/_ten022/temp/TempApplicationTests.java
@@ -0,0 +1,13 @@
+package com._ten022.temp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
