7973ad4cf6e930e2fb7d5f3e6f3f17da7eba9d61
Hyeok22 <wnsgur347@gmail.com>
2025-03-01 20:46:30 +0900

    포트폴리오 프리뷰/키트 저장 및 출력

--- frontend/src/pages/BrandingResult/Preview/portfolioPreview.js
+++ frontend/src/pages/BrandingResult/Preview/portfolioPreview.js
@@ -2,27 +2,26 @@ import React from "react";
 
 import "./portfolioPreview.css"
 
-const PortfolioPreview = ({ kitColor, description, hashtags }) => {
+const PortfolioPreview = ({ kitColor, tagline, hashtags }) => {
     return (
-        <div className="portfolio-preview-container">
-            <div className="portfolio-preview-kit"
-                style={{ background: `linear-gradient(to left, ${kitColor}, #ffffff 120%)` }}>
-                <div className="portfolio-preview-content">
+        <>
+            <p className="portfolio-kit-standard">1920 x 1080</p>
+            <div className="portfolio-preview-container">
+                <div className="portfolio-preview-kit"
+                    style={{ background: `linear-gradient(to right, ${kitColor}, #ffffff 120%)` }}>
+                    <div className="portfolio-preview-intro">Portfolio</div>
                     <h1 className="portfolio-preview-title">
-                        코드 속에 삶을 담는 개발자, <span className="name-highlight">이용우</span>입니다.
+                        {tagline}
                     </h1>
-                    <p className="portfolio-preview-description">
-                        {description}
-                    </p>
-                </div>
-                <div className="portfolio-preview-hashtag-list">
-                    {hashtags.map((hashtag) => (
-                        <div className="portfolio-preview-hashtag-chip" style={{color: kitColor}}>{hashtag}</div>
-                    ))}
+                    <div className="portfolio-preview-hashtag-list">
+                        {hashtags.map((hashtag) => (
+                            <div className="portfolio-preview-hashtag-chip" style={{ color: kitColor }}>{hashtag}</div>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
-export default BasicPreview;
+export default PortfolioPreview;
