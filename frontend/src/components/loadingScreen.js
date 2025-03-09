2704543d8b6fbc1a5889ad3d1a658ad351f22a1e
Hyeok22 <wnsgur347@gmail.com>
2025-03-09 23:33:56 +0900

    스테퍼 및 로딩 페이지 반응형 구현

--- frontend/src/components/loadingScreen.js
+++ frontend/src/components/loadingScreen.js
@@ -6,10 +6,10 @@ import "./loadingScreen.css";
 const LoadingScreen = ({ currentStep, message }) => {
 
   return (
-    <div className="loading2-body"> 
-      <p className="loading2-text" > { currentStep === 1 ? '제니오가 매의 눈으로 이력서를 살펴보고 있어요. 👀' : '제니오가 맞춤형 브랜딩 키트를 만들고 있어요. 🛠' } </p>
-      <p className="loading2-subtext"> { message } </p>
-      <img src={currentStep === 1 ? loadingImage1 : loadingImage2} alt="로딩 중" className="loading-image1" />
+    <div className="loading-body"> 
+      <p className="loading-text" > { currentStep === 1 ? '제니오가 매의 눈으로 이력서를 살펴보고 있어요. 👀' : '제니오가 맞춤형 브랜딩 키트를 만들고 있어요. 🛠' } </p>
+      <p className="loading-subtext"> 제니오가 총 nnnn자의 텍스트를 읽고 있어요. </p>
+      <img className="loading-image" src={currentStep === 1 ? loadingImage1 : loadingImage2} alt="로딩 중" />
     </div>
   );
 };
